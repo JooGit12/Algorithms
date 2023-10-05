1a48e59428526f46b5e9110493dc7676689e1ecf
JooGit12 <137463638+JooGit12@users.noreply.github.com>
2023-10-05 14:21:21 +0900

    [unrated] Title: 더 크게 합치기, Time: 1.69 ms, Memory: 73.4 MB -BaekjoonHub

--- 프로그래머스/unrated/181939. 더 크게 합치기/더 크게 합치기.java
+++ 프로그래머스/unrated/181939. 더 크게 합치기/더 크게 합치기.java
@@ -9,3 +9,19 @@ class Solution {
         return Math.max(result1, result2);
     }
 }
+
+//class Solution15 {
+//    public int solution(int a, int b) {
+//        String aabb = String.valueOf(a) + String.valueOf(b);
+//        String bbaa = String.valueOf(b) + String.valueOf(a);
+//        
+//        int result1 = Integer.parseInt(aabb);
+//        int result2 = Integer.parseInt(bbaa);
+//        
+//        if (result1 >= result2) {
+//            return result1;
+//        } else {
+//            return result2;
+//        }
+//    }
+//}
