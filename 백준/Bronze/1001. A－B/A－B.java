d51842437684dbd2d981468c9fbc84abaf3ad31c
JooGit12 <137463638+JooGit12@users.noreply.github.com>
2023-10-08 16:43:09 +0900

    [Bronze V] Title: A-B, Time: 212 ms, Memory: 17764 KB -BaekjoonHub

---
+++ 백준/Bronze/1001. A－B/A－B.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner k = new Scanner(System.in);
+		int a = k.nextInt();
+		int b = k.nextInt();
+		System.out.println(a - b);
+	}
+}
