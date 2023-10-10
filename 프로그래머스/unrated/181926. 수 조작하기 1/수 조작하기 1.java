d6f77b99c5d42839b915731ee8fd686bfd2cb5f9
Joo Won Kim <joocode12@gmail.com>
2023-10-10 14:42:36 +0900

    [level unrated] Title: 수 조작하기 1, Time: 1.76 ms, Memory: 74.7 MB -BaekjoonHub

--- 프로그래머스/unrated/181926. 수 조작하기 1/수 조작하기 1.java
+++ 프로그래머스/unrated/181926. 수 조작하기 1/수 조작하기 1.java
@@ -1,18 +1,5 @@
 class Solution {
     public int solution(int n, String control) {
-        StringBuilder sb = new StringBuilder(control);
-        for (int i = 0; i < control.length(); i++) {
-        	char c = sb.charAt(i);
-			if (c == 'w') {
-				n += 1;
-			} else if (c == 's'){
-				n -= 1;
-			} else if (c == 'd') {
-				n += 10;
-			} else {
-				n -= 10;
-			}
-		}
-        return n;
+        return control.chars().reduce(n, (acc, c) -> acc + (c == 'w' ? 1 : c == 's' ? -1 : c == 'd' ? 10 : -10));
     }
 }
