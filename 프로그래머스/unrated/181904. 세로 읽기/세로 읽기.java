2d02e26231aad0afdc993224d58249058c0b58df
Joo Won Kim <joocode12@gmail.com>
2023-10-25 15:11:20 +0900

    [level unrated] Title: 세로 읽기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/unrated/181904. 세로 읽기/세로 읽기.java
+++ 프로그래머스/unrated/181904. 세로 읽기/세로 읽기.java
@@ -1,9 +1,11 @@
 class Solution {
-	public String solution(String my_string, int m, int c) {
-		StringBuilder sb = new StringBuilder();
-		for (int i = c - 1; i < my_string.length(); i += m) {
-			sb.append(my_string.charAt(i));
-		}
-		return sb.toString();
-	}
+
+    public String solution(String my_string, int m, int c) {
+        String answer = "";
+
+        for (int i = c - 1; i < my_string.length(); i += m) {
+            answer += my_string.charAt(i);
+        }
+        return answer;
+    }
 }
