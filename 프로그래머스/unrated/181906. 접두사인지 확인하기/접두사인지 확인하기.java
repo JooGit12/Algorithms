d05c8d32953b9ca73319d29343984913755830b7
Joo Won Kim <joocode12@gmail.com>
2023-10-29 16:26:58 +0900

    [level unrated] Title: 접두사인지 확인하기, Time: 0.02 ms, Memory: 74.7 MB -BaekjoonHub

--- 프로그래머스/unrated/181906. 접두사인지 확인하기/접두사인지 확인하기.java
+++ 프로그래머스/unrated/181906. 접두사인지 확인하기/접두사인지 확인하기.java
@@ -1,15 +1,5 @@
-import java.util.ArrayList;
-
 class Solution {
     public int solution(String my_string, String is_prefix) {
-        
-        ArrayList<String> prefixList = new ArrayList<>();
-        
-        for (int i = 0; i < my_string.length(); i++) {
-			prefixList.add(my_string.substring(0, i));
-		}
-        
-        return prefixList.contains(is_prefix) ? 1 : 0;
-        
+        return my_string.startsWith(is_prefix) ? 1 : 0;
     }
 }
