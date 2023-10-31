a058844259d5073827b0f7d6e1aa272ac69a6f53
Joo Won Kim <joocode12@gmail.com>
2023-10-31 15:32:22 +0900

    [level unrated] Title: 뒤에서 5등까지, Time: 9.35 ms, Memory: 72.3 MB -BaekjoonHub

--- 프로그래머스/unrated/181853. 뒤에서 5등까지/뒤에서 5등까지.java
+++ 프로그래머스/unrated/181853. 뒤에서 5등까지/뒤에서 5등까지.java
@@ -1,7 +1,10 @@
 import java.util.Arrays;
 
 class Solution {
-	public int[] solution(int[] num_list) {
-		return Arrays.copyOf(Arrays.stream(num_list).sorted().toArray(), 5);
-	}
+    public int[] solution(int[] num_list) {
+        return Arrays.stream(num_list)
+            .sorted()
+            .limit(5)
+            .toArray();
+    }
 }
