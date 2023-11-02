b676bde740d445893cf00c93338c6601cb1b0bdb
Joo Won Kim <joocode12@gmail.com>
2023-11-02 14:11:26 +0900

    [level unrated] Title: 카운트 다운, Time: 0.05 ms, Memory: 77.8 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181899. 카운트 다운/카운트 다운.java
@@ -0,0 +1,14 @@
+import java.util.ArrayList;
+import java.util.List;
+
+class Solution {
+    public List<Integer> solution(int start, int end_num) {
+        List<Integer> answer = new ArrayList<>();
+        
+        for (int i = start; i >= end_num; i--) {
+            answer.add(i);
+        }
+        
+        return answer;
+    }
+}
