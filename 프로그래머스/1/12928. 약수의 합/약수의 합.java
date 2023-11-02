7e970475bf5af12977e3f95800d36c2ed49540ed
Joo Won Kim <joocode12@gmail.com>
2023-11-02 17:01:56 +0900

    [level 1] Title: 약수의 합, Time: 0.08 ms, Memory: 79.2 MB -BaekjoonHub

--- 프로그래머스/1/12928. 약수의 합/약수의 합.java
+++ 프로그래머스/1/12928. 약수의 합/약수의 합.java
@@ -1,17 +1,11 @@
 class Solution {
-    public int solution(int n) {
-        
-        int answer = 0;
-        
-        for (int i = 1; i <= Math.sqrt(n); i++) {
-			if (n % i == 0) {
+	public int solution(int num) {
+		int answer = 0;
+		for (int i = 1; i <= num / 2; i++) {
+			if (num % i == 0) {
 				answer += i;
-				if (i != n / i) {
-					answer += n / i;
-				}
 			}
 		}
-        
-        return answer;
-    }
+		return answer + num;
+	}
 }
