c7276711a084841e0d34823afcc240bbf31b07aa
Joo Won Kim <joocode12@gmail.com>
2023-11-03 21:25:59 +0900

    [level 1] Title: 하샤드 수, Time: 1.09 ms, Memory: 73 MB -BaekjoonHub

--- 프로그래머스/1/12947. 하샤드 수/하샤드 수.java
+++ 프로그래머스/1/12947. 하샤드 수/하샤드 수.java
@@ -1,13 +1,6 @@
 class Solution {
     public boolean solution(int x) {
-    	
-        String xToString = Integer.toString(x);
-        int hashard = 0;
-        
-        for (int i = 0; i < xToString.length(); i++) {
-			hashard += Character.getNumericValue(xToString.charAt(i));
-		}
-        
-        return x % hashard == 0;
+        int sum = String.valueOf(x).chars().map(ch -> ch - '0').sum();
+        return x % sum == 0;
     }
 }
