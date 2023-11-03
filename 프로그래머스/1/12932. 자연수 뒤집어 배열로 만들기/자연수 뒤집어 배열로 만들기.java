7b5bca86b0178cdcb19afba22740d466535023e4
Joo Won Kim <joocode12@gmail.com>
2023-11-03 14:42:16 +0900

    [level 1] Title: 자연수 뒤집어 배열로 만들기, Time: 0.03 ms, Memory: 72.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/12932. 자연수 뒤집어 배열로 만들기/자연수 뒤집어 배열로 만들기.java
@@ -0,0 +1,15 @@
+import java.util.ArrayList;
+
+class Solution {
+    public ArrayList<Integer> solution(long n) {
+        
+    	ArrayList<Integer> arr = new ArrayList<>();
+        
+        while (n > 0) {
+			arr.add((int)(n % 10));
+			n /= 10;
+		}
+        
+        return arr;
+    }
+}
