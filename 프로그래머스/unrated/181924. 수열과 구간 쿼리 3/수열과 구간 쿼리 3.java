956d43ae25b6ccf6478a5630fd4aaf08821c85fd
Joo Won Kim <joocode12@gmail.com>
2023-11-12 16:12:08 +0900

    [level unrated] Title: 수열과 구간 쿼리 3, Time: 0.04 ms, Memory: 79 MB -BaekjoonHub

--- 프로그래머스/unrated/181924. 수열과 구간 쿼리 3/수열과 구간 쿼리 3.java
+++ 프로그래머스/unrated/181924. 수열과 구간 쿼리 3/수열과 구간 쿼리 3.java
@@ -1,19 +1,18 @@
-import java.util.ArrayList;
-import java.util.Collections;
+import java.util.Arrays;
 
 class Solution {
     public int[] solution(int[] arr, int[][] queries) {
-    	
-    	ArrayList<Integer> intList = new ArrayList<>(arr.length);
-    	
-    	for (int i = 0; i < arr.length; i++) {
-			intList.add(arr[i]);
-		}
-    	
-    	for (int i = 0; i < queries.length; i++) {
-			Collections.swap(intList, queries[i][0], queries[i][1]);
-		}
-    	
-    	return intList.stream().mapToInt(Integer::intValue).toArray();
+        int[] answer = Arrays.copyOf(arr, arr.length);
+
+        for (int[] query : queries) {
+            int i = query[0];
+            int j = query[1];
+
+            int temp = answer[i];
+            answer[i] = answer[j];
+            answer[j] = temp;
+        }
+
+        return answer;
     }
 }
