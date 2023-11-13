9903efd91697ec5c6aff14ba6a8cdb7eeceedf6f
Joo Won Kim <joocode12@gmail.com>
2023-11-13 16:56:12 +0900

    [level unrated] Title: 가까운 1 찾기, Time: 0.03 ms, Memory: 70.4 MB -BaekjoonHub

--- 프로그래머스/unrated/181898. 가까운 1 찾기/가까운 1 찾기.java
+++ 프로그래머스/unrated/181898. 가까운 1 찾기/가까운 1 찾기.java
@@ -2,12 +2,12 @@ class Solution {
     public int solution(int[] arr, int idx) {
         int answer = -1;
         
-        for (int i = 0; i < arr.length; i++) {
-			if (arr[i] == 1 && i >= idx) {
-				answer = i;
-				break;
-			}
-		}
+        for(int i = idx; i<arr.length; i++){
+            if(arr[i]==1){
+                answer = i;
+                break;
+            }
+        }
         
         return answer;
     }
