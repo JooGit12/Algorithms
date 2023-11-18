831a8d32243a84992e1c679a9f54d0ca42429f90
Joo Won Kim <joocode12@gmail.com>
2023-11-18 15:01:38 +0900

    [level unrated] Title: 분수의 덧셈, Time: 0.15 ms, Memory: 83.9 MB -BaekjoonHub

--- 프로그래머스/unrated/120808. 분수의 덧셈/분수의 덧셈.java
+++ 프로그래머스/unrated/120808. 분수의 덧셈/분수의 덧셈.java
@@ -1,31 +1,16 @@
 class Solution {
-    public int[] solution(int numer1, int denom1, int numer2, int denom2) {
-        int[] answer = new int[2];
-
-        int gcd = 0;
-        int bigger = Math.max(denom1, denom2);
-        int smaller = Math.min(denom1, denom2);
-
-        while (smaller != 0) {
-            gcd = smaller;
-            smaller = bigger % smaller;
-            bigger = gcd;
+    public int[] solution(int denum1, int num1, int denum2, int num2) {
+        int mother = num1 * num2;
+        int son1 = num1 * denum2;
+        int son2 = num2 * denum1;
+        int totalSon = son1 + son2;
+        for(int i = mother; i >= 1; i--){
+            if(totalSon % i == 0 && mother % i == 0){
+                totalSon /= i;
+                mother /= i;
+            }
         }
-
-        answer[1] = denom1 * denom2 / gcd;
-        answer[0] = numer1 * (answer[1] / denom1) + numer2 * (answer[1] / denom2);
-
-        int num = answer[0];
-        int den = answer[1];
-        while (den != 0) {
-            gcd = den;
-            den = num % den;
-            num = gcd;
-        }
-
-        answer[0] /= gcd;
-        answer[1] /= gcd;
-
+        int[] answer = {totalSon, mother};
         return answer;
     }
 }
