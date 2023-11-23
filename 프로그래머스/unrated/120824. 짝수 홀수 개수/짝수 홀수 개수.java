38d0381b1a42dd209968a2e93b3111076979940e
Joo Won Kim <joocode12@gmail.com>
2023-11-23 20:38:38 +0900

    [level unrated] Title: 짝수 홀수 개수, Time: 0.01 ms, Memory: 74.3 MB -BaekjoonHub

--- 프로그래머스/unrated/120824. 짝수 홀수 개수/짝수 홀수 개수.java
+++ 프로그래머스/unrated/120824. 짝수 홀수 개수/짝수 홀수 개수.java
@@ -1,12 +1,9 @@
 class Solution {
     public int[] solution(int[] num_list) {
-    	int even = 0;
-    	
-        for (int i = 0; i < num_list.length; i++) {
-			if (num_list[i] % 2 == 0) even++;
-		}
+        int[] answer = new int[2];
 
-        int[] answer = {even, num_list.length - even};
+        for(int i = 0; i < num_list.length; i++)
+            answer[num_list[i] % 2]++;
 
         return answer;
     }
