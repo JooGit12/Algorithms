c672ec6ff5e6025aedc72ebb2dc73b93aaa85c33
Joo Won Kim <joocode12@gmail.com>
2023-11-23 20:05:23 +0900

    [level unrated] Title: 중앙값 구하기, Time: 0.42 ms, Memory: 74.4 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/120811. 중앙값 구하기/중앙값 구하기.java
@@ -0,0 +1,8 @@
+import java.util.Arrays;
+
+class Solution {
+    public int solution(int[] array) {
+    	Arrays.sort(array);
+        return array.length % 2 == 0 ? array[array.length / 2 - 1] : array[array.length / 2];
+    }
+}
