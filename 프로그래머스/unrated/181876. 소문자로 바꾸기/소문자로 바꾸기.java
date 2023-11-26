38b8606ab7ef97dc84cd2e1720c8683f0d3c199e
Joo Won Kim <joocode12@gmail.com>
2023-11-26 16:49:38 +0900

    [level unrated] Title: 소문자로 바꾸기, Time: 0.02 ms, Memory: 74 MB -BaekjoonHub

--- 프로그래머스/unrated/181876. 소문자로 바꾸기/소문자로 바꾸기.java
+++ 프로그래머스/unrated/181876. 소문자로 바꾸기/소문자로 바꾸기.java
@@ -1,16 +1,6 @@
 class Solution {
     public String solution(String myString) {
-    	
-    	StringBuilder sb = new StringBuilder();
-    	
-        for (int i = 0; i < myString.length(); i++) {
-        	
-			if (Character.isUpperCase(myString.charAt(i))) {
-				sb.append(Character.toLowerCase(myString.charAt(i)));
-			}else {
-				sb.append(myString.charAt(i));
-			}
-		}
-        return sb.toString();
+
+        return myString.toLowerCase();
     }
 }
