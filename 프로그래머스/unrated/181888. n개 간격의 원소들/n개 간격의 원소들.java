1fc1272a39bedfbb9e108cbf1264c9fb17f98184
Joo Won Kim <joocode12@gmail.com>
2023-11-27 17:38:21 +0900

    [level unrated] Title: n개 간격의 원소들, Time: 0.01 ms, Memory: 77.2 MB -BaekjoonHub

--- 프로그래머스/unrated/181888. n개 간격의 원소들/n개 간격의 원소들.java
+++ 프로그래머스/unrated/181888. n개 간격의 원소들/n개 간격의 원소들.java
@@ -1,13 +1,10 @@
-import java.util.ArrayList;
-
 class Solution {
     public int[] solution(int[] num_list, int n) {
-        ArrayList<Integer> arr = new ArrayList<>();
-        
-        for (int i = 0; i < num_list.length; i += n) {
-			arr.add(num_list[i]);
-		}
-        
-        return arr.stream().mapToInt(Integer::intValue).toArray();
+        int[] answer = new int[num_list.length % n == 0 ? num_list.length / n : num_list.length / n + 1];
+        for (int i = 0; i < answer.length; i++) {
+            answer[i] = num_list[i * n];
+        }
+        return answer;
     }
+
 }
