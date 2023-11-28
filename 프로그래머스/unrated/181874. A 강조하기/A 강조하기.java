53239d4d819ec9587990f653575181c44ad17023
Joo Won Kim <joocode12@gmail.com>
2023-11-28 23:11:14 +0900

    [level unrated] Title: A 강조하기, Time: 0.06 ms, Memory: 77.9 MB -BaekjoonHub

--- 프로그래머스/unrated/181874. A 강조하기/A 강조하기.java
+++ 프로그래머스/unrated/181874. A 강조하기/A 강조하기.java
@@ -1,6 +1,12 @@
 class Solution {
 	public String solution(String myString) {
 		myString = myString.toLowerCase();
-		return myString.replaceAll("a", "A");
+		StringBuilder sb = new StringBuilder(myString);
+		
+		for (int i = 0; i < myString.length(); i++) {
+			if (myString.charAt(i) == 'a') sb.setCharAt(i, 'A');
+		}
+		
+		return sb.toString();
 	}
 }
