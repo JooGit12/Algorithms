329f4d44f655fdf267fe179c0bcdd3f7cccee306
Joo Won Kim <joocode12@gmail.com>
2023-11-30 20:32:58 +0900

    [level unrated] Title: 숫자 찾기, Time: 0.03 ms, Memory: 73.7 MB -BaekjoonHub

--- 프로그래머스/unrated/120904. 숫자 찾기/README.md
+++ 프로그래머스/unrated/120904. 숫자 찾기/README.md
@@ -1,10 +1,10 @@
 # [level unrated] 숫자 찾기 - 120904 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120904) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120904#) 
 
 ### 성능 요약
 
-메모리: 82.3 MB, 시간: 7.88 ms
+메모리: 73.7 MB, 시간: 0.03 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2023년 11월 4일 20:29:52
+2023년 11월 4일 20:32:55
 
 ### 문제 설명
 
--- 프로그래머스/unrated/120904. 숫자 찾기/숫자 찾기.java
+++ 프로그래머스/unrated/120904. 숫자 찾기/숫자 찾기.java
@@ -1,16 +1,9 @@
 class Solution {
     public int solution(int num, int k) {
-    	int answer = 0;    	
-    	String a = num + "";
-    	String b = k + "";
-    	
-    	for (int i = 0; i < a.length(); i++) {
-			if (a.charAt(i) == b.charAt(0)) {
-				answer = i + 1;
-                break;
-			}
-		}
-    	
-    	return (answer > 0) ? answer : - 1;
+        String numStr = String.valueOf(num);
+        String kStr = String.valueOf(k);
+
+        int answer = numStr.indexOf(kStr);
+        return answer < 0 ? -1 : answer + 1 ;
     }
 }
