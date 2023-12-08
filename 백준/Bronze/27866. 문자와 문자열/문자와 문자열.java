3d037c94bcf116b3e08b068023fec878eb34d681
Joo Won Kim <joocode12@gmail.com>
2023-12-08 13:06:32 +0900

    [Bronze V] Title: 문자와 문자열, Time: 208 ms, Memory: 17752 KB -BaekjoonHub

---
+++ 백준/Bronze/27866. 문자와 문자열/문자와 문자열.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+class Main {
+	public static void main(String[] args) {
+		Scanner k = new Scanner(System.in);
+		Scanner l = new Scanner(System.in);
+		
+		System.out.println(k.next().charAt(k.nextInt() - 1));
+	}
+}
