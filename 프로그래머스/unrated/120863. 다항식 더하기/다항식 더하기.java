6f80df08cd3958e113ea135506add713f30b8db8
Joo Won Kim <joocode12@gmail.com>
2023-12-26 15:54:24 +0900

    [level unrated] Title: 다항식 더하기, Time: 6.44 ms, Memory: 74.9 MB -BaekjoonHub

--- 프로그래머스/unrated/120863. 다항식 더하기/다항식 더하기.java
+++ 프로그래머스/unrated/120863. 다항식 더하기/다항식 더하기.java
@@ -1,36 +1,29 @@
 class Solution {
     public String solution(String polynomial) {
+        String answer = "";        
         int num = 0;
         int countX = 0;
         
         String[] polynomialArray = polynomial.split("\\s+\\+\\s+");
         
-        for (String term : polynomialArray) {
-            if (term.contains("x")) {
-                countX += processX(term);
-            } else {
-                num += Integer.parseInt(term);
-            }
-        }
-
-        return formatAnswer(countX, num);
-    }
-
-    private int processX(String term) {
-        if (term.length() == 1) {
-            return 1;
-        } else {
-            return Integer.parseInt(term.replaceAll("x", ""));
-        }
-    }
-
-    private String formatAnswer(int countX, int num) {
+        for (int i = 0; i < polynomialArray.length; i++) {
+			if (!polynomialArray[i].contains("x")) {
+				num += Integer.parseInt(polynomialArray[i]);
+			}else if (polynomialArray[i].contains("x") && polynomialArray[i].length() == 1) {
+				countX++;
+			}else {
+				countX += Integer.parseInt(polynomialArray[i].replaceAll("x", ""));
+			}
+		}
+        
+        
         if (countX == 0) {
-            return Integer.toString(num);
-        } else if (countX == 1) {
-            return "x" + (num == 0 ? "" : " + " + num);
-		} else {
-            return countX + "x" + (num == 0 ? "" : " + " + num);
-        }
+			return num + "";
+		}else if (num == 0) {
+			return countX + "x";
+		}else {
+			return countX + "x + " + num;
+		}
+        
     }
 }
