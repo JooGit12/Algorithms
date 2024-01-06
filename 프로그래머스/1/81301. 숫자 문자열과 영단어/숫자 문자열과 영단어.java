30415ecc3c537a1b1121bcd85e88e0f7db1e5e60
Joo Won Kim <joocode12@gmail.com>
2024-01-06 16:30:18 +0900

    [level 1] Title: 숫자 문자열과 영단어, Time: 0.07 ms, Memory: 72.9 MB -BaekjoonHub

--- 프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.java
+++ 프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.java
@@ -1,9 +1,35 @@
+import java.util.HashMap;
+import java.util.Map;
+
 class Solution {
     public int solution(String s) {
-        String[] strArr = {"zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"};
-        for(int i = 0; i < strArr.length; i++) {
-            s = s.replaceAll(strArr[i], i + "");
+        Map<String, Integer> wordToNumber = new HashMap<>();
+        wordToNumber.put("zero", 0);
+        wordToNumber.put("one", 1);
+        wordToNumber.put("two", 2);
+        wordToNumber.put("three", 3);
+        wordToNumber.put("four", 4);
+        wordToNumber.put("five", 5);
+        wordToNumber.put("six", 6);
+        wordToNumber.put("seven", 7);
+        wordToNumber.put("eight", 8);
+        wordToNumber.put("nine", 9);
+
+        StringBuilder numberString = new StringBuilder();
+        StringBuilder word = new StringBuilder();
+
+        for (char c : s.toCharArray()) {
+            if (Character.isDigit(c)) {
+                numberString.append(c);
+            } else {
+                word.append(c);
+                if (wordToNumber.containsKey(word.toString())) {
+                    numberString.append(wordToNumber.get(word.toString()));
+                    word.setLength(0);
+                }
+            }
         }
-        return Integer.parseInt(s);
+
+        return Integer.parseInt(numberString.toString());
     }
 }
