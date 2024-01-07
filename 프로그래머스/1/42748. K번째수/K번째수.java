26211e5b2a55ad2acb0217c894b47ff47ef5aa9b
Joo Won Kim <joocode12@gmail.com>
2024-01-07 14:58:58 +0900

    [level 1] Title: K번째수, Time: 0.32 ms, Memory: 71.1 MB -BaekjoonHub

--- 프로그래머스/1/42748. K번째수/K번째수.java
+++ 프로그래머스/1/42748. K번째수/K번째수.java
@@ -3,25 +3,13 @@ import java.util.Arrays;
 class Solution {
     public int[] solution(int[] array, int[][] commands) {
         int[] answer = new int[commands.length];
-        
-        for (int i = 0; i < commands.length; i++) {
-			 int [] cuttedArr = cutArray(array, commands[i][0], commands[i][1]);
-			 answer[i] = sortArray(cuttedArr, commands[i][2]);
-		}
-        
+
+        for(int i=0; i<commands.length; i++){
+            int[] temp = Arrays.copyOfRange(array, commands[i][0]-1, commands[i][1]);
+            Arrays.sort(temp);
+            answer[i] = temp[commands[i][2]-1];
+        }
+
         return answer;
-    }
-    
-    private int sortArray (int array[], int c) {
-    	Arrays.sort(array);
-    	return array[c - 1];
-    }
-    
-    private int[] cutArray (int array[], int a, int b) {
-    	int [] cuttedArr = new int[b - a + 1];
-    	for (int i = a - 1, j = 0; i < b; i++, j++) {
-			cuttedArr[j] = array[i];
-		}
-    	return cuttedArr;
     }
 }
