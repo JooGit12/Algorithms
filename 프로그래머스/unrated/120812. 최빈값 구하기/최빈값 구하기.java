570bd0df7b30fc26b6c4aff532c79a01f6f2b6e7
Joo Won Kim <joocode12@gmail.com>
2024-01-09 15:55:13 +0900

    [level unrated] Title: 최빈값 구하기, Time: 0.20 ms, Memory: 69.2 MB -BaekjoonHub

--- 프로그래머스/unrated/120812. 최빈값 구하기/최빈값 구하기.java
+++ 프로그래머스/unrated/120812. 최빈값 구하기/최빈값 구하기.java
@@ -3,31 +3,20 @@ import java.util.Map;
 
 class Solution {
     public int solution(int[] array) {
-    	HashMap<Integer, Integer> modeMap = new HashMap<>();
-    	
-    	// 각 정수의 빈도수 계산
-    	for (int i : array) {
-			if (modeMap.containsKey(i)) {
-				modeMap.put(i, modeMap.get(i) + 1);
-			}else {
-				modeMap.put(i, 1);
-			}
-		}
-    	
-    	// 최빈값 찾기
-    	int mode = array[0];
-    	int maxCount = 0;
-    	boolean multipleModes = false;
-    	for (Map.Entry<Integer, Integer> entry : modeMap.entrySet()) {
-			if (entry.getValue() > maxCount) {
-				maxCount = entry.getValue();
-				mode = entry.getKey();
-				multipleModes = false;
-			} else if (entry.getValue() == maxCount) {
-				multipleModes = true;
-			}
-		}
-    	
-    	return multipleModes ? - 1 : mode;
+        int maxCount = 0;
+        int answer = 0;
+        Map<Integer, Integer> map = new HashMap<>();
+        for(int number : array){
+            int count = map.getOrDefault(number, 0) + 1;
+            if(count > maxCount){
+                maxCount = count;
+                answer = number;
+            }
+            else  if(count == maxCount){
+                answer = -1;
+            }
+            map.put(number, count);
+        }
+        return answer;
     }
 }
