d7ca6adbec0548c9d0c680e988b3ecd990af5f50
Joo Won Kim <joocode12@gmail.com>
2024-01-13 17:47:36 +0900

    [level Lv.1] Title: 카드 뭉치, Time: 0.18 ms, Memory: 82.1 MB -BaekjoonHub

--- 프로그래머스/Lv.1/159994. 카드 뭉치/카드 뭉치.java
+++ 프로그래머스/Lv.1/159994. 카드 뭉치/카드 뭉치.java
@@ -1,28 +1,32 @@
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.Stack;
 
 class Solution {
     public String solution(String[] cards1, String[] cards2, String[] goal) {
-        Stack<String> cardsStack1 = new Stack<>();
-        Stack<String> cardsStack2 = new Stack<>();
+        Stack<String> cardsStack1 = convertArrayToStack(cards1);
+        Stack<String> cardsStack2 = convertArrayToStack(cards2);
         
-        for (int i = cards1.length - 1; i >= 0; i--) {
-			cardsStack1.push(cards1[i]);
-		}
-        
-        for (int i = cards2.length - 1; i >= 0; i--) {
-			cardsStack2.push(cards2[i]);
-		}
-        
-        for (int i = 0; i < goal.length; i++) {
-			if (!cardsStack1.isEmpty() && cardsStack1.peek().equals(goal[i])) {
-				cardsStack1.pop();
-			} else if (!cardsStack2.isEmpty() && cardsStack2.peek().equals(goal[i])) {
-				cardsStack2.pop();
-			}else {
-				return "No";
-			}
-		}
-        
-		return "Yes";
-	}
+        return isMatchGoal(cardsStack1, cardsStack2, goal) ? "Yes" : "No";
+    }
+    
+    private Stack<String> convertArrayToStack(String[] array) {
+        Stack<String> stack = new Stack<>();
+        stack.addAll(Arrays.asList(array));
+        Collections.reverse(stack);
+        return stack;
+    }
+
+    private boolean isMatchGoal(Stack<String> stack1, Stack<String> stack2, String[] goal) {
+        for (String target : goal) {
+            if (!stack1.isEmpty() && stack1.peek().equals(target)) {
+                stack1.pop();
+            } else if (!stack2.isEmpty() && stack2.peek().equals(target)) {
+                stack2.pop();
+            } else {
+                return false;
+            }
+        }
+        return true;
+    }
 }
