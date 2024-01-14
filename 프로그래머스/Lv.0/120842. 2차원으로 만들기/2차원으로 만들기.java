5fc2e55d14cb374b6d6711c6366da4efa3b9e42d
Joo Won Kim <joocode12@gmail.com>
2024-01-14 17:12:11 +0900

    [level Lv.0] Title: 2차원으로 만들기, Time: 0.01 ms, Memory: 74.9 MB -BaekjoonHub

--- 프로그래머스/Lv.0/120842. 2차원으로 만들기/2차원으로 만들기.java
+++ 프로그래머스/Lv.0/120842. 2차원으로 만들기/2차원으로 만들기.java
@@ -1,8 +1,6 @@
 class Solution {
     public int[][] solution(int[] num_list, int n) {
-        int[][] answer = {};
-
-        answer = new int[num_list.length/n][n];
+        int[][] answer = new int[num_list.length/n][n];
 
         for(int i=0; i<num_list.length; i++){
             answer[i/n][i%n]=num_list[i];
