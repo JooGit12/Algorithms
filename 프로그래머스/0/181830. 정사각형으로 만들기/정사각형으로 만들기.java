c839dc081d3fe42e6c7acbc707a1f3e816995568
Joo Won Kim <joocode12@gmail.com>
2024-01-18 14:43:51 +0900

    [level 0] Title: 정사각형으로 만들기, Time: 0.10 ms, Memory: 84.4 MB -BaekjoonHub

--- 프로그래머스/0/181830. 정사각형으로 만들기/정사각형으로 만들기.java
+++ 프로그래머스/0/181830. 정사각형으로 만들기/정사각형으로 만들기.java
@@ -1,14 +1,11 @@
 class Solution {
     public int[][] solution(int[][] arr) {
-    	int arrLength = Math.max(arr.length, arr[0].length);
-    	int[][] answer = new int[arrLength][arrLength];
-        
-    	for (int i = 0; i < arr.length; i++) {
-			for (int j = 0; j < arr[0].length; j++) {
-				answer[i][j] = arr[i][j];
-			}
-		}
-        
-        return answer;
+        int max = Math.max(arr.length, arr[0].length);
+        int[][] array = new int[max][max];
+
+        for (int i = 0; i < arr.length; i++) {
+            System.arraycopy(arr[i], 0, array[i], 0, arr[0].length);
+        }
+        return array;
     }
 }
