aa69919399cccd38f606cbdeb80dce4beb36346e
Joo Won Kim <joocode12@gmail.com>
2024-01-21 00:21:19 +0900

    [level 1] Title: 푸드 파이트 대회, Time: 11.26 ms, Memory: 86.8 MB -BaekjoonHub

--- 프로그래머스/1/134240. 푸드 파이트 대회/푸드 파이트 대회.java
+++ 프로그래머스/1/134240. 푸드 파이트 대회/푸드 파이트 대회.java
@@ -1,17 +1,13 @@
 class Solution {
     public String solution(int[] food) {
-        StringBuilder sb = new StringBuilder();
-        
-        for (int i = 1; i < food.length; i++) {
-        	for (int j = 0; j < food[i] / 2; j++) {
-        		sb.append(i + "");				
-			}
-		}
-        
-        String sooWoongFood = sb.toString();
-        
-        StringBuilder challengerFood = sb.reverse();
-        
-        return sooWoongFood + "0" + challengerFood;
+        String answer = "0";
+
+        for (int i = food.length - 1; i > 0; i--) {
+            for (int j = 0; j < food[i] / 2; j++) {
+                answer = i + answer + i; 
+            }
+        }
+
+        return answer;
     }
 }
