799755cc82c671561a17494ff338d25dbbbc7f6a
Joo Won Kim <joocode12@gmail.com>
2024-01-22 19:55:02 +0900

    [level 0] Title: 삼각형의 완성조건 (2), Time: 0.02 ms, Memory: 76.1 MB -BaekjoonHub

--- 프로그래머스/0/120868. 삼각형의 완성조건 （2）/삼각형의 완성조건 （2）.java
+++ 프로그래머스/0/120868. 삼각형의 완성조건 （2）/삼각형의 완성조건 （2）.java
@@ -1,10 +1,5 @@
 class Solution {
     public int solution(int[] sides) {
-    	int longgerSide = Math.max(sides[0], sides[1]);
-    	int shorterSide = Math.min(sides[0], sides[1]);
-    	
-        int answer = (longgerSide + shorterSide) - (longgerSide - shorterSide) - 1;
-        
-        return answer;
+        return Math.min(sides[0], sides[1]) * 2 - 1;
     }
 }
