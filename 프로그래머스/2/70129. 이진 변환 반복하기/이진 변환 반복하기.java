f390d03fb6cf149cd5780ba6acf768ff68a13c98
Joo Won Kim <joocode12@gmail.com>
2024-01-24 18:03:49 +0900

    [level 2] Title: 이진 변환 반복하기, Time: 5.61 ms, Memory: 68.7 MB -BaekjoonHub

--- 프로그래머스/2/70129. 이진 변환 반복하기/이진 변환 반복하기.java
+++ 프로그래머스/2/70129. 이진 변환 반복하기/이진 변환 반복하기.java
@@ -1,25 +1,30 @@
 class Solution {
 	public int[] solution(String s) {
-
 		int countZero = 0;
 		int countNum = 0;
 
 		while (!s.equals("1")) {
-			int countOne = 0;
 			countNum++;
-			
-			for (int i = 0; i < s.length(); i++) {
-				if (s.charAt(i) == '0') {
-					countZero++;
-				}else {
-					countOne++;
-				}
-			}
-			s = Integer.toBinaryString(countOne);
+			int[] result = countZeroAndOne(s);
+			countZero += result[0];
+			s = Integer.toBinaryString(result[1]);
 		}
 
-		int[] answer = { countNum, countZero };
+		return new int[] { countNum, countZero };
+	}
 
-		return answer;
+	private int[] countZeroAndOne(String s) {
+		int countZero = 0;
+		int countOne = 0;
+
+		for (int i = 0; i < s.length(); i++) {
+			if (s.charAt(i) == '0') {
+				countZero++;
+			} else {
+				countOne++;
+			}
+		}
+
+		return new int[] { countZero, countOne };
 	}
 }
