79bc3ce11f99868598e1e17c5cafd06a22e076e6
Joo Won Kim <joocode12@gmail.com>
2024-01-25 18:00:08 +0900

    [level 2] Title: 다음 큰 숫자, Time: 0.02 ms, Memory: 51.5 MB -BaekjoonHub

--- 프로그래머스/2/12911. 다음 큰 숫자/다음 큰 숫자.java
+++ 프로그래머스/2/12911. 다음 큰 숫자/다음 큰 숫자.java
@@ -1,6 +1,7 @@
 class Solution {
     public int solution(int n) {
-        int countOne = Integer.bitCount(n);
+    	// 비트 연산자 사용
+    	int countOne = Integer.bitCount(n); // n을 2진수로 변환 후 1의 개수 반환
         for (int i = n + 1; ; i++) {
             if (Integer.bitCount(i) == countOne) {
                 return i;
