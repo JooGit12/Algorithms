d07f7b829995cd3a615853bf3e5c81f8956f2f8d
Joo Won Kim <joocode12@gmail.com>
2024-01-30 15:31:17 +0900

    [level 1] Title: 문자열 내 마음대로 정렬하기, Time: 3.82 ms, Memory: 76.5 MB -BaekjoonHub

---
+++ 프로그래머스/1/12915. 문자열 내 마음대로 정렬하기/문자열 내 마음대로 정렬하기.java
@@ -0,0 +1,13 @@
+import java.util.Arrays;
+import java.util.Comparator;
+
+class Solution {
+    public String[] solution(String[] strings, int n) {
+        
+        Arrays.sort(strings, Comparator
+        		.comparing((String str) -> str.charAt(n))
+        		.thenComparing(Comparator.naturalOrder()));
+                
+        return strings;
+    }
+}
