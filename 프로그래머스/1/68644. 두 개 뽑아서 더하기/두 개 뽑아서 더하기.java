94cf70de1d01816773075155bfc888164764e18a
Joo Won Kim <joocode12@gmail.com>
2024-02-07 17:05:50 +0900

    [level 1] Title: 두 개 뽑아서 더하기, Time: 3.89 ms, Memory: 75.1 MB -BaekjoonHub

---
+++ 프로그래머스/1/68644. 두 개 뽑아서 더하기/두 개 뽑아서 더하기.java
@@ -0,0 +1,17 @@
+import java.util.HashSet;
+import java.util.Set;
+
+class Solution {
+    public int[] solution(int[] numbers) {
+    	
+    	Set<Integer> numSet = new HashSet<>();
+
+    	for (int i = 0; i < numbers.length; i++) {
+			for (int j = i + 1; j < numbers.length; j++) {
+				numSet.add(numbers[i] + numbers[j]);
+			}
+		}
+    	
+    	return numSet.stream().sorted().mapToInt(Integer::intValue).toArray();
+    }
+}
