96675deeb601074fbfa52c874984ad7d734a76ea
Joo Won Kim <joocode12@gmail.com>
2024-02-29 17:37:30 +0900

    [level 2] Title: 기능개발, Time: 2.96 ms, Memory: 73.8 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/기능개발.java
+++ 프로그래머스/2/42586. 기능개발/기능개발.java
@@ -1,38 +1,26 @@
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Queue;
 
 class Solution {
-    private int[] calculateEndDays(int[] progresses, int[] speeds) {
-        int[] daysArr = new int[progresses.length];
-        for (int i = 0; i < progresses.length; i++) {
-            int days = (100 - progresses[i]) % speeds[i] == 0 ?
-                    (100 - progresses[i]) / speeds[i] :
-                    (100 - progresses[i]) / speeds[i] + 1;
-            daysArr[i] = days;
-        }
-        return daysArr;
-    }
-
-    private List<Integer> calculateDeployCount(int[] daysArr) {
-        List<Integer> deployCountList = new ArrayList<>();
-        int deployDay = daysArr[0];
-        int deployCount = 1;
-        for (int i = 1; i < daysArr.length; i++) {
-            if (daysArr[i] <= deployDay) {
-                deployCount++;
-            } else {
-                deployCountList.add(deployCount);
-                deployDay = daysArr[i];
-                deployCount = 1;
-            }
-        }
-        deployCountList.add(deployCount);
-        return deployCountList;
-    }
-
     public int[] solution(int[] progresses, int[] speeds) {
-        int[] daysArr = calculateEndDays(progresses, speeds);
-        List<Integer> deployCountList = calculateDeployCount(daysArr);
-        return deployCountList.stream().mapToInt(Integer::intValue).toArray();
+        Queue<Integer> q = new LinkedList<>();
+        List<Integer> answerList = new ArrayList<>();
+
+        for (int i = 0; i < speeds.length; i++) {
+            int date = (int) Math.ceil((100 - progresses[i]) / (double) speeds[i]);
+
+            if (!q.isEmpty() && q.peek() < date) {
+                answerList.add(q.size());
+                q.clear();
+            }
+
+            q.offer(date);
+        }
+
+        answerList.add(q.size());
+
+        return answerList.stream().mapToInt(Integer::intValue).toArray();
     }
 }
