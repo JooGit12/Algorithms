d9e8a7f29dd410d08052a6bd2e28e5df4c98739c
Joo Won Kim <joocode12@gmail.com>
2024-03-02 17:24:11 +0900

    [level 2] Title: 멀리 뛰기, Time: 0.10 ms, Memory: 75.5 MB -BaekjoonHub

---
+++ 프로그래머스/2/12914. 멀리 뛰기/멀리 뛰기.java
@@ -0,0 +1,14 @@
+class Solution {
+    public long solution(int n) {
+        long[] fibo = new long[2001];
+        
+        fibo[1] = 1;
+        fibo[2] = 2;
+        
+        for (int i = 3; i <= n; i++) {
+			fibo[i] = (fibo[i - 1] + fibo[i - 2]) % 1234567;
+		}
+        
+        return fibo[n];
+    }
+}
