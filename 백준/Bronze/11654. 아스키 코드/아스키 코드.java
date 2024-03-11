afff7bb84642f36f5de478d83d3e19ddf05280ca
Joo Won Kim <joocode12@gmail.com>
2024-03-11 15:55:47 +0900

    [Bronze V] Title: 아스키 코드, Time: 196 ms, Memory: 17660 KB -BaekjoonHub

---
+++ 백준/Bronze/11654. 아스키 코드/아스키 코드.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+    	Scanner k = new Scanner(System.in);
+    	char inputChar = k.next().charAt(0);
+    	System.out.println((int)inputChar);
+    	k.close();
+    }
+}
