ad047d5812e22f5b3dd3f21b15b938094a5e7ab5
Joo Won Kim <joocode12@gmail.com>
2024-03-27 10:57:39 +0900

    [Silver III] Title: 프린터 큐, Time: 156 ms, Memory: 15404 KB -BaekjoonHub

---
+++ 백준/Silver/1966. 프린터 큐/프린터 큐.java
@@ -0,0 +1,62 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.LinkedList;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		
+		int testLen = Integer.parseInt(br.readLine());
+		
+		for (int i = 0; i < testLen; i++) {
+			String[] inputs = br.readLine().split(" ");
+			int docsLen = Integer.parseInt(inputs[0]);
+			int docIndex = Integer.parseInt(inputs[1]);
+			
+			LinkedList<int[]> q = new LinkedList<>();
+			String[] docs = br.readLine().split(" ");
+			
+			for (int j = 0; j < docsLen; j++) {
+				q.offer(new int[] {j, Integer.parseInt(docs[j])});
+			}
+			
+			int count = 0;
+			
+			while (!q.isEmpty()) {
+				int[] front = q.poll();
+				boolean isMax = true;
+				
+				for (int j = 0; j < q.size(); j++) {
+					if (front[1] < q.get(j)[1]) {
+						q.offer(front);
+						for (int k = 0; k < j; k++) {
+							q.offer(q.poll());
+						}
+						
+						isMax = false;
+						break;
+					}
+				}
+				
+				if (!isMax) {
+					continue;
+				}
+				count++;
+				
+				if (front[0] == docIndex) {
+					break;
+				}
+			}
+			bw.write(Integer.toString(count));
+			bw.newLine();
+		}
+		
+		bw.flush();
+		bw.close();
+		br.close();
+	}
+}
