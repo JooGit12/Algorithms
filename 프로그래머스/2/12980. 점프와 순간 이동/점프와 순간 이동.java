77606859e03efd00a2d26030c4744eb92452c3f3
Joo Won Kim <joocode12@gmail.com>
2024-04-07 13:32:19 +0900

    [level 2] Title: 점프와 순간 이동, Time: 0.02 ms, Memory: 51.8 MB -BaekjoonHub

--- 프로그래머스/2/12980. 점프와 순간 이동/점프와 순간 이동.java
+++ 프로그래머스/2/12980. 점프와 순간 이동/점프와 순간 이동.java
@@ -1,12 +1,5 @@
 public class Solution {
 	public int solution(int n) {
-		String binaryN = Integer.toBinaryString(n);
-		int answer = 0;
-		
-		for (char ch : binaryN.toCharArray()) {
-			if (ch == '1') answer++;
-		}
-		
-		return answer;
+		return Integer.bitCount(n);
 	}
 }
