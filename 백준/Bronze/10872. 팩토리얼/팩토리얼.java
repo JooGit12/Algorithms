54b69523197d22443eeccb10bae3cac0343b768f
Joo Won Kim <joocode12@gmail.com>
2024-04-20 18:13:18 +0900

    [Bronze III] Title: 팩토리얼, Time: 124 ms, Memory: 14160 KB -BaekjoonHub

--- 백준/Bronze/10872. 팩토리얼/팩토리얼.java
+++ 백준/Bronze/10872. 팩토리얼/팩토리얼.java
@@ -1,25 +1,18 @@
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
 
 public class Main {
-	public static void main(String[] args) {
-		try (BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-				BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out))) {
-
-			int n = Integer.parseInt(br.readLine());
-			long factorial = 1;
-
-			for (int i = 2; i <= n; i++) {
-				factorial *= i;
-			}
-
-			bw.write(String.valueOf(factorial));
-			bw.flush();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-	}
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        
+        int N = Integer.parseInt(reader.readLine());
+        
+        System.out.println(factorial(N));
+    }
+    
+    private static int factorial (int n) {
+        if (n < 2) return 1; // 종료 지점
+        return n * factorial(n - 1);
+    }
 }
