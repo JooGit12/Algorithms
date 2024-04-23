13bc0fdc019a7f8496966b1f85d707fb6b0827bb
Joo Won Kim <joocode12@gmail.com>
2024-04-23 11:02:00 +0900

    [Silver III] Title: 등수 매기기, Time: 672 ms, Memory: 54920 KB -BaekjoonHub

--- 백준/Silver/2012. 등수 매기기/등수 매기기.java
+++ 백준/Silver/2012. 등수 매기기/등수 매기기.java
@@ -1,28 +1,26 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
+import java.util.PriorityQueue;
+import java.util.Queue;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));        
+        Queue<Integer> pq = new PriorityQueue<Integer>();
         
         int N = Integer.parseInt(reader.readLine());
-        int[] ranks = new int[N];
         long answer = 0;
         
-        for (int i = 0; i < ranks.length; i++) {
-            ranks[i] = Integer.parseInt(reader.readLine());
+        for (int i = 0; i < N; i++) {
+            pq.offer(Integer.parseInt(reader.readLine()));
         }
         
-        Arrays.sort(ranks);
-        
-        for (int i = 0; i < ranks.length; i++) {
-            answer += Math.abs(ranks[i] - i - 1);
+        for (int i = 0; i < N; i++) {
+            answer += Math.abs(pq.poll() - i - 1);
         }
         
         System.out.println(answer);
-        
         reader.close();
     }
 }
