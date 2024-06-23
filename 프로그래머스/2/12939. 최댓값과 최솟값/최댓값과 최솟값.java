a60b734f3bd8c03c39050054bb3ab45c53d2c6eb
Joo Won Kim <joocode12@gmail.com>
2024-06-23 19:56:22 +0900

    [level 2] Title: 최댓값과 최솟값, Time: 13.10 ms, Memory: 73.1 MB -BaekjoonHub

--- 프로그래머스/2/12939. 최댓값과 최솟값/최댓값과 최솟값.java
+++ 프로그래머스/2/12939. 최댓값과 최솟값/최댓값과 최솟값.java
@@ -2,15 +2,15 @@ import java.util.Arrays;
 
 class Solution {
     public String solution(String s) {
-        String[] sArr = s.split(" ");
-        int[] iArr = new int[sArr.length];
+        String[] stringArray = s.split(" ");
+        int[] intArray = new int[stringArray.length];
         
-        for (int i = 0; i < iArr.length; i++) {
-			iArr[i] = Integer.parseInt(sArr[i]);
-		}
+        for (int i = 0; i < intArray.length; i++) {
+            intArray[i] = Integer.parseInt(stringArray[i]);
+        }
         
-        Arrays.sort(iArr);
+        Arrays.sort(intArray);
         
-        return iArr[0] + " " + iArr[iArr.length - 1];
+        return intArray[0] + " " + intArray[intArray.length - 1];
     }
 }
