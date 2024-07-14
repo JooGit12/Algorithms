700a3c3cca419e0cc0742078bf73574fae32a30e
Joo Won Kim <joocode12@gmail.com>
2024-07-14 14:55:36 +0900

    [level 2] Title: 피보나치 수, Time: 2.33 ms, Memory: 74 MB -BaekjoonHub

--- 프로그래머스/2/12945. 피보나치 수/피보나치 수.java
+++ 프로그래머스/2/12945. 피보나치 수/피보나치 수.java
@@ -1,14 +1,14 @@
 class Solution {
-	public int solution(int n) {
-
-		long[] memo = new long[n + 1];
-		memo[0] = 0;
-		memo[1] = 1;
-
-		for (int i = 2; i <= n; i++) {
-			memo[i] = (memo[i - 1] + memo[i - 2]) % 1234567;
-		}
-
-		return (int) (memo[n] % 1234567);
-	}
+    public int solution(int n) {
+        
+        int[] fibo = new int[n + 1];
+        fibo[0] = 0;
+        fibo[1] = 1;
+        
+        for(int i = 2; i <= n; i++) {
+            fibo[i] = (fibo[i - 1] + fibo[i - 2]) % 1234567;
+        }
+        
+        return fibo[n];
+    }
 }
