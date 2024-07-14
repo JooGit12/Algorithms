8522bf35fbf9946d021c19acec3238f5699e7e36
Joo Won Kim <joocode12@gmail.com>
2024-07-14 15:13:48 +0900

    [level 2] Title: 구명보트, Time: 10.09 ms, Memory: 53.7 MB -BaekjoonHub

--- 프로그래머스/2/42885. 구명보트/구명보트.java
+++ 프로그래머스/2/42885. 구명보트/구명보트.java
@@ -1,22 +1,25 @@
-import java.util.Arrays;
+import java.util.*;
+import java.lang.*;
 
 class Solution {
-	public int solution(int[] people, int limit) {
-		int answer = 0;
-
-		Arrays.sort(people);
-
-		int lightIndex = 0;
-		int heavyIndex = people.length - 1;
-
-		while (lightIndex <= heavyIndex) {
-			if (people[lightIndex] + people[heavyIndex] <= limit) {
-				lightIndex++;
-			}
-			heavyIndex--;
-			answer++;
-		}
-
-		return answer;
-	}
+    public int solution(int[] people, int limit) {
+        int answer = 0;
+        Arrays.sort(people);
+        
+        int lightIndex = 0;
+        int heavyIndex = people.length - 1;
+        
+        while(lightIndex <= heavyIndex) {
+            if(people[lightIndex] + people[heavyIndex] <= limit) {
+                heavyIndex--;
+                lightIndex++;
+                answer++;
+            } else {
+                heavyIndex--;
+                answer++;
+            }
+        }
+        
+        return answer;
+    }
 }
